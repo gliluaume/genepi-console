b9b3125ca9cddaef793b72e09f2527a04f792315
guillaume <salicisguillaume@yahoo.fr>
2018-07-07 22:27:01 +0200

    test: improve coverage

--- tests/console-outputter.spec.js
+++ tests/console-outputter.spec.js
@@ -2,11 +2,63 @@
 
 const { ConsoleOutputter } = require('../lib/console-outputter')
 
+// console shortcuts
+const ESC = '\u001B['
+const cursorLeft = ESC + 'G'
+const eraseEndLine = ESC + 'K'
+
 describe('consoleOutputter', () => {
   it('has required functions', async function() {
     const outputter = new ConsoleOutputter()
     expect(outputter.header).toBeInstanceOf(Function)
     expect(outputter.inner).toBeInstanceOf(Function)
     expect(outputter.footer).toBeInstanceOf(Function)
+  })
+
+  describe('header', () => {
+    it('does not write anything', () => {
+      const spyWrite = jest.spyOn(process.stdout, 'write')
+      const outputter = new ConsoleOutputter()
+      outputter.header()
+      try {
+        expect(spyWrite).not.toBeCalled()
+      } finally {
+        spyWrite.mockReset()
+      }
+    })
+  })
+
+  describe('inner', () => {
+    it('print expected string', () => {
+      const spyWrite = jest.spyOn(process.stdout, 'write')
+      const cursorPosition = ESC + 14 + 'C'
+      const wordRepr = 'h\u001B[31me\u001B[39mllo'
+      const expected = cursorLeft + eraseEndLine + cursorPosition + wordRepr
+
+      const outputter = new ConsoleOutputter()
+      outputter.inner('hello', 1)
+      try {
+        expect(spyWrite).toBeCalled()
+        expect(spyWrite).toBeCalledWith(expected)
+      } finally {
+        spyWrite.mockReset()
+      }
+    })
+  })
+
+  describe('footer', () => {
+    it('clean line a carriage return', () => {
+      const spyWrite = jest.spyOn(process.stdout, 'write')
+      const expected = cursorLeft + eraseEndLine + '\n'
+
+      const outputter = new ConsoleOutputter()
+      outputter.footer()
+      try {
+        expect(spyWrite).toBeCalled()
+        expect(spyWrite).toBeCalledWith(expected)
+      } finally {
+        spyWrite.mockReset()
+      }
+    })
   })
 })
--- tests/detect-source.spec.js
+++ tests/detect-source.spec.js
@@ -2,10 +2,30 @@
 
 const path = require('path')
 const execa = require('execa')
+const sampleUrl = 'https://raw.githubusercontent.com/gliluaume/genepi-console/master/tests/text.txt'
+const samplePath = path.join(__dirname, './text.txt')
 
-describe('detectSource', () => {
+const {
+  detectSource,
+  SUPPORTED_SOURCES } = require('../lib/detect-source')
+
+describe('detectSource from args', () => {
+  it('can guess stdin source', () => {
+    expect(detectSource([])).toEqual(SUPPORTED_SOURCES.stdin)
+  })
+  it('can guess string source', () => {
+    expect(detectSource(['My text is beautiful'])).toEqual(SUPPORTED_SOURCES.string)
+  })
+  it('can guess http source', () => {
+    expect(detectSource([sampleUrl])).toEqual(SUPPORTED_SOURCES.http)
+  })
+  it('can guess file source', () => {
+    expect(detectSource([samplePath])).toEqual(SUPPORTED_SOURCES.file)
+  })
+})
+
+describe('pipeSource (WARN this resembles a functionnal test)', () => {
   const fixturePath = path.join(__dirname, './fixture-detect-source.js')
-  const samplePath = path.join(__dirname, './text.txt')
   const expected = 'This is a text.'
 
   it('can guess that a filename is given', async function() {
@@ -15,6 +35,11 @@ describe('detectSource', () => {
 
   it('can guess that a string is given', async function() {
     const { stdout } = await execa(fixturePath, [expected])
+    return expect(stdout).toEqual(expected)
+  })
+
+  it('can guess that a url is given', async function() {
+    const { stdout } = await execa(fixturePath, [sampleUrl])
     return expect(stdout).toEqual(expected)
   })
 })
